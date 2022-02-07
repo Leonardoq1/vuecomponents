966be369420897bc3739284e5297c8f2a7bc9b4f
Brayan Leonardo Alvarez Reyes <90276381+Leonardoq1@users.noreply.github.com>
2022-02-06 22:34:22 -0600

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,59 @@
+<template>
+  <!-- Buttons-->
+  <div>
+    <div>
+      <b-button>Button</b-button>
+      <b-button variant="danger">Button</b-button>
+      <b-button variant="success">Button</b-button>
+      <b-button variant="outline-primary">Button</b-button>
+    </div>
+    <!--  Collapse-->
+    <div>
+      <b-button v-b-toggle.collapse-1 variant="primary"
+        >Toggle Collapse</b-button
+      >
+      <b-collapse id="collapse-1" class="mt-2">
+        <b-card>
+          <p class="card-text">Collapse contents Here</p>
+          <b-button v-b-toggle.collapse-1-inner size="sm"
+            >Toggle Inner Collapse</b-button
+          >
+          <b-collapse id="collapse-1-inner" class="mt-2">
+            <b-card>Hello!</b-card>
+          </b-collapse>
+        </b-card>
+      </b-collapse>
+    </div>
+    <!--Dropdown-->
+    <div>
+      <b-dropdown id="dropdown-1" text="Dropdown Button" class="m-md-2">
+        <b-dropdown-item>First Action</b-dropdown-item>
+        <b-dropdown-item>Second Action</b-dropdown-item>
+        <b-dropdown-item>Third Action</b-dropdown-item>
+        <b-dropdown-divider></b-dropdown-divider>
+        <b-dropdown-item active>Active action</b-dropdown-item>
+        <b-dropdown-item disabled>Disabled action</b-dropdown-item>
+      </b-dropdown>
+    </div>
+
+    <div>
+      <label for="example-datepicker">Choose a date</label>
+      <b-form-datepicker
+        id="example-datepicker"
+        v-model="value"
+        class="mb-2"
+      ></b-form-datepicker>
+      <p>Value: '{{ value }}'</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: "",
+    };
+  },
+};
+</script>
